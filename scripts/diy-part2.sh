90f3606a9cda48fd14d44f4c7ead261484e44155
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2025-02-17 09:27:51 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -10,8 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
-# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# 修改openwrt登陆地址,把下面的 192.168.100.1 修改成你想要的就可以了
+# sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # 修改子网掩码
 #sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
@@ -488,6 +488,8 @@ CONFIG_PACKAGE_procps-ng-ps=y
 echo "
 CONFIG_GRUB_IMAGES=y
 CONFIG_VMDK_IMAGES=y
+CONFIG_ISO_IMAGES=y
+CONFIG_QCOW2_IMAGES=y
 " >> .config
 
 # 添加设备
